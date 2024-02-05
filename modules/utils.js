e98e98a28bcdc9270176b1a151f1421730a52551
Kevin <k.lagergren@live.se>
2024-02-05 11:05:47 +0100

    feat: Add createPostItNote and integrate dynamic deletion

---
+++ modules/utils.js
@@ -0,0 +1,33 @@
+import { deleteNote } from "./firebase/crudfirebase.js";
+import { displayNotes } from "./display.js";
+
+function createPostItNote(noteId, title, content) {
+  const postItTitle = document.createElement("h2");
+  postItTitle.textContent = title;
+  const postItContent = document.createElement("p");
+  postItContent.textContent = content;
+  const note = document.createElement("div");
+  note.classList.add("post-it");
+  note.appendChild(postItTitle);
+  note.appendChild(postItContent);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("post-it__delete");
+  deleteBtn.onclick = () => {
+    deleteNote(noteId)
+      .then(() => {
+        displayNotes();
+        console.log("Note deleted:", noteId);
+      })
+      .catch((error) => {
+        console.error("Error removing note: ", error);
+      });
+  };
+  note.appendChild(deleteBtn);
+
+  const container = document.querySelector(".post-it__list");
+  container.appendChild(note);
+}
+
+export { createPostItNote };
