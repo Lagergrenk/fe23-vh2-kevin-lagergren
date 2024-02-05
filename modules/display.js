9c1303fa508282edfc6e97987f5e2ee61e323777
Kevin <k.lagergren@live.se>
2024-02-05 11:03:18 +0100

    feat: Implementing displayNotes for dynamic UI update

---
+++ modules/display.js
@@ -0,0 +1,14 @@
+import { getNotes } from "./firebase/crudfirebase.js";
+import { createPostItNote } from "./utils.js";
+
+async function displayNotes() {
+  const notes = await getNotes();
+  const notesContainer = document.querySelector(".post-it__list");
+  notesContainer.innerHTML = "";
+
+  Object.entries(notes).forEach(([noteId, noteData]) => {
+    createPostItNote(noteId, noteData.title, noteData.content);
+  });
+}
+
+export { displayNotes };
